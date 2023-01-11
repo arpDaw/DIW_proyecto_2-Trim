8cd6898d1e17dae4fb99d2ac3492a6e4b56d5755
Pablo <pabloruizalfaro@gmail.com>
2023-01-11 14:29:09 +0100

    ionic

---
+++ prueba/newApp/src/app/clientes/clientes.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.page.html',
+  styleUrls: ['./clientes.page.scss'],
+})
+export class ClientesPage implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  redHome(){
+    this.router.navigate(['/home'])
+  }
+
+}
