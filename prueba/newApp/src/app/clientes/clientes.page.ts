e19d71821a1a1f6849c85686ddfa15e0214fc5b2
Pablo <pabloruizalfaro@gmail.com>
2023-01-12 09:36:13 +0100

    1.8 y 1.9

--- prueba/newApp/src/app/clientes/clientes.page.ts
+++ prueba/newApp/src/app/clientes/clientes.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-clientes',
@@ -8,13 +10,33 @@ import { Router } from '@angular/router';
 })
 export class ClientesPage implements OnInit {
 
-  constructor(private router:Router) { }
+  users: any = [];
+
+  constructor(
+    private router:Router,
+    private http:HttpClient) {
+
+   }
 
   ngOnInit() {
+    
+    this.getUsers().subscribe(res=>{
+      console.log("Res", res)
+      this.users = res;
+    });
   }
 
   redHome(){
     this.router.navigate(['/home'])
   }
 
+  getUsers(){
+    return this.http
+    .get("./assets/files/clientes.json")
+    .pipe(
+      map((res:any) => { 
+        return res.data;
+      })
+      )
+  }
 }
