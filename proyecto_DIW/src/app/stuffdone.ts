6382ffae5451c6b5e3a1a3adfe4f805d7e22159d
Pablo <pabloruizalfaro@gmail.com>
2023-02-09 10:22:23 +0100

    journal and home updates

---
+++ proyecto_DIW/src/app/stuffdone.ts
@@ -0,0 +1,4 @@
+export interface Stuffdone {
+    stuff:string
+}
+
--- proyecto_DIW/src/app/tab2/tab2.page.ts
+++ proyecto_DIW/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contribution } from '../contribution';
+import { Stuffdone } from '../stuffdone';
 
 @Component({
   selector: 'app-tab2',
@@ -6,9 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  @Input() new:Contribution ={
+    content:""
+  }
+
+  @Input() new2:Stuffdone = {
+    stuff:""
+  }
 
   constructor() {}
 
-  contributions = ["derp"]
+  todayDate = new Date();
+  
+  contributions:string[] = []
+  stuffdone:string[] = []
 
+  addContribution(){
+    if(this.new.content.trim().length === 0){
+      return
+    }
+    this.contributions.push(this.new.content)
+
+    this.new.content = ""
+  }
+
+  addStuffDone(){
+    if(this.new2.stuff.trim().length === 0){
+      return
+    }
+    this.stuffdone.push(this.new2.stuff)
+
+    this.new2.stuff = ""
+  }
 }
