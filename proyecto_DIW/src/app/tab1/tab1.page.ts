53f00cc7c7b8d3c4bc1bf66be25d761f9a3565c3
Pablo <pabloruizalfaro@gmail.com>
2023-02-15 14:25:28 +0100

    alert

--- proyecto_DIW/src/app/tab1/tab1.page.ts
+++ proyecto_DIW/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, private navCtrl: NavController) {}
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'Glad you had a great day!',
+      subHeader: 'Want to write about it?',
+      cssClass: 'custom-aler',
+      buttons: [
+        {
+          text: 'Maybe later',
+          cssClass: 'alert-button-cancel',
+          handler: () => {
+            this.navCtrl.navigateForward('tabs/tab2')
+          }
+        },
+        {
+          text: 'Sure! :D',
+          cssClass: 'alert-button-confirm'
+        }
+      ]
+    })
+    await alert.present()
+    let result = await alert.onDidDismiss()
+    console.log(result)
+  }
+
+
 
 }
